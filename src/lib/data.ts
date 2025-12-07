74b0068a33622c37672149879e30a94a39e38323
daboutt <baotran2115@gmail.com>
2025-12-07 15:13:53 +0700

    fix: update path for img

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,6 @@
-import newJeans1 from '../../public/njz-1.gif';
-import newJeans2 from '../../public/njz-2.gif';
-import newJeans3 from '../../public/njz-3.gif';
+import newJeans1 from '/njz-1.gif';
+import newJeans2 from '/njz-2.gif';
+import newJeans3 from '/njz-3.gif';
 import type { ArtistPreview } from './types';
 
 export const data: Record<string, ArtistPreview[]> = {
