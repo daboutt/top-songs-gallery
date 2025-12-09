5ef0d06e73bde4ab8e5c4bffa5daa845838225a5
daboutt <baotran2115@gmail.com>
2025-12-09 21:27:10 +0700

    feat: update styles

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,21 @@
 import type { Artist } from './types';
 
+export const ANIMATION_CONFIG = {
+  initial: {
+    scaleY: 1.15,
+  },
+  hover: {
+    scaleY: 1.3,
+    scaleX: 1.3,
+  },
+  transition: {
+    type: 'spring',
+    stiffness: 300,
+    damping: 10,
+    mass: 0.8,
+  },
+} as const;
+
 export const ARTIST_TITLES: Artist[] = [
   { id: 'newJeans', displayName: 'NewJeans' },
   { id: 'lowG', displayName: 'LowG' },
